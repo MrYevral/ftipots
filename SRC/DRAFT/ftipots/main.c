2cb8715c6c8dc6ba8c762cf3172bfd1f2b22b4e8
Harvey Clark <hac22@aber.ac.uk>
2013-11-21 11:19:17 +0000

    creating a netbeans project as per the spec

---
+++ SRC/DRAFT/ftipots/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: hac22
+ *
+ * Created on 21 November 2013, 11:16
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
