404a1a01c7cb339442a9022a9f99650292b09980
Harvey Clark <hac22@aber.ac.uk>
2013-11-28 17:16:02 +0000

    generic testing

---
+++ Draft Code/main.c
@@ -0,0 +1,26 @@
+struct rescue_assestes{
+    /*callsign = alphanumeric no gaps
+     * assest type = single letter L = life boat H = helecopter
+     * placemnt name, in alphanumeric string
+     * longitude latitude in degress
+     * max speed knots
+     * max deploy time mins
+     * restock time mins
+     */
+    
+    
+};
+
+
+
+
+int main(int argc, char** argv) {
+	printf("Hello, World!");
+        
+        
+        
+        
+    return (EXIT_SUCCESS);
+}
+
+
