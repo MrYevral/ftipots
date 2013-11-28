c0623b19a0867f877683bc17f06db446afeed733
Harvey Clark <hac22@aber.ac.uk>
2013-11-28 11:41:45 +0000

    adding netbeans

---
+++ NetBeans/Peril/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: hac22
+ *
+ * Created on 28 November 2013, 11:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
