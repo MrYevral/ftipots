e48bb65ef254cf53a04054799e79a530e2d3d8fe
Harvey Clark <hac22@aber.ac.uk>
2013-12-11 23:00:26 +0000

    stuff

--- Draft Code/rescue_asset.c
+++ Draft Code/rescue_asset.c
@@ -60,12 +60,44 @@ void print_assets(rescue_asset *root){
 		current = current->next;
 	}
 }
-rescue_asset *responding(char type,location *destination,int time_needed){
-	if(type == "l"){
+rescue_asset *responding(char type,location *destination,int time_needed,rescue_asset *root){
+	rescue_asset *current;
+	rescue_asset *out;
+	current = root;
+	out = NULL;
+	double min_deploy = 900000;
+	double temp;
+	double asset_deploy = 900000;
+	if(type == "h"){
+		while(current !=NULL){
+			if(current->asset_type =="h"||current->asset_type =="H"){
+				temp = great_circle(current->base_location,*destination);
+				min_deploy = (temp/current->speed)*2+time_needed;
+				if(min_deploy<current->max_deploy && min_deploy<asset_deploy ){
+					out = current;
+					asset_deploy = min_deploy;
+				}
+			}
+			current = current->next;
+		}
+		return current;
+	}
+	else{
+		while(current !=NULL){
+			if(current->asset_type == "l"||current->asset_type == "L"){
+				temp = great_circle(current->base_location,destination);
+				asset_max=(current->max_deploy_time/2/current->speed;
+				if( asset_max>temp && asset_max<distance ) {
+					out = current;
+					distance = asset_max;
+				}
+			}
+			current = current->next;
+
 
 	}
 
-
+	return current;
 
 
 }
--- Draft Code/time.c
+++ Draft Code/time.c
@@ -8,18 +8,28 @@
  * takes two time structs
  */
 
-int time_past(time *a,time *b){
+int time_past(time a,time b){
 	int a_sec;
 	int b_sec;
+	int out;
 	a_sec = to_sec(a);
 	b_sec = to_sec(b);
+	if(a_sec>b_sec){
+		out = a_sec-b_sec;
 
+	}
+	else{
+		out = b_sec-a_sec;
+	}
 
-
+	out/=60;
+	return out;
 }
-int to_sec(time *in){
+int to_sec(time in){
 	int out;
 	out = 0;
-	out += in->sec;
-
+	out += in.sec;
+	out += in.min*60;
+	out += in.hour*3600;
+	return out;
 }
